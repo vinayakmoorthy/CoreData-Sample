e661c1350090a8542a1f7c6a7fcf32f39256f1d6
Vinayak moorthy <moorthyvina@gmail.com>
2017-10-25 20:54:52 +0530

    -Added new project

---
+++ CodedataSample/CodedataSample/Testmodal.swift
@@ -0,0 +1,19 @@
+//
+//  UserValue.swift
+//  CodedataSample
+//
+//  Created by Jegadhisan on 12/09/17.
+//  Copyright © 2017 changepond. All rights reserved.
+//
+
+import UIKit
+
+import CoreData
+import Foundation
+
+class Testmodal: NSManagedObject {
+  
+  @NSManaged var name: String?
+  @NSManaged var age: String?
+
+}
